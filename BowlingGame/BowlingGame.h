4c1b34862b7cad2c076c6dfa9a9504b97e7dd60c
Samuel Stoltzfus <samuel@superiorplastic.net>
2020-09-02 10:02:39 -0600

    initial commit

---
+++ BowlingGame/BowlingGame.h
@@ -0,0 +1,19 @@
+//
+//  BowlingGame.h
+//  BowlingGame
+//
+//  Created by Samuel Stoltzfus on 9/2/20.
+//  Copyright © 2020 Superior Plastic Products, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowlingGame.
+FOUNDATION_EXPORT double BowlingGameVersionNumber;
+
+//! Project version string for BowlingGame.
+FOUNDATION_EXPORT const unsigned char BowlingGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingGame/PublicHeader.h>
+
+
