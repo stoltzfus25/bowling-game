431b6726de68f792ae03bf1f2b95e21537fa46f2
Samuel Stoltzfus <samuel@superiorplastic.net>
2020-09-02 10:07:48 -0600

    test gutter game scores zero

---
+++ BowlingGame/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  BowlingGame
+//
+//  Created by Samuel Stoltzfus on 9/2/20.
+//  Copyright © 2020 Superior Plastic Products, Inc. All rights reserved.
+//
+
+import Foundation
+
+final class Game {
+    func roll(_ pins: Int) {
+        
+    }
+    
+    func score() -> Int {
+        return 0
+    }
+}
